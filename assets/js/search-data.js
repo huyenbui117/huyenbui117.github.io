f7839f1d7b42f57c1111e86e9f28cc25f92d6dfa
huyenbui117 <62584585+huyenbui117@users.noreply.github.com>
2025-12-20 16:59:12 +0000

    Deploying to gh-pages from @ huyenbui117/huyenbui117.github.io@f2bd0a41f072414f907854d8aff5541f0fe524f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-resume",
+          title: "resume",
+          description: "My academic and professional journey in AI research and engineering, with a focus on multi-agent systems, multimodal generative AI, and scientific discovery.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
